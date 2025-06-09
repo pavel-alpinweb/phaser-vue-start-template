6698d4a0a995d7b3d56d0de71399c1c65bb73643
Павел Бездорнов <alpinweb@yandex.ru>
2025-06-09 10:06:04 +0600

    init project

---
+++ src/scenes/game.scene.js
@@ -0,0 +1,14 @@
+export class GameScene extends Phaser.Scene {
+    constructor() {
+        super('MainScene')
+    }
+
+    preload() {
+        this.load.image('player', 'assets/img/player.png') // из public/
+    }
+
+    create() {
+        this.add.text(100, 100, 'Hello from Phaser!', { fontSize: '32px', fill: '#fff' })
+        this.add.image(400, 300, 'player')
+    }
+}
---
+++ src/screens/Game.screen.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { onMounted, ref } from 'vue'
+import Phaser from 'phaser'
+import GameScene from '@/scenes/game.scene';
+
+const gameContainer = ref(null)
+
+onMounted(() => {
+  new Phaser.Game({
+    type: Phaser.AUTO,
+    width: 960,
+    height: 540,
+    parent: gameContainer.value,
+    scene: new GameScene(),
+    backgroundColor: '#000000',
+  })
+})
+</script>
+
+<template>
+  <div class="game-screen">
+    <div ref="gameContainer" class="game-screen__game-wrapper"></div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
